7196aa9b4c957d8e032fe144faa3e2f94a1ba55a
Kaplin86 <87865073+Kaplin86@users.noreply.github.com>
2024-12-22 20:00:45 -0500

    WIP MElee

--- Scripts/Culor.gd
+++ Scripts/Culor.gd
@@ -2,41 +2,93 @@ extends CharacterBody2D
 class_name BaseCulor
 var isMouseHovering = false
 
-var Mypersrandom
-var Timesincestart : float
+var Mypersrandom # used for animations we dont want synced
+var Timesincestart : float # time since start
 
 @export var culor : Color
 @export var speed = 40
 @export var health = 20
-@export var range = 40
+@export var attackRange = 60
+@export var attackCooldown = 2
+@export var CurrentState = "idle"
 
+var attackTimer = Timer.new()
 
 var rng = RandomNumberGenerator.new()
 var targetposition : Vector2
 var targetcutoff = 0
+var walkinganimstate = false
+var lastpos = Vector2(0,0)
+
+@onready var Animator = AnimationPlayer.new()
 
 func _ready():
-	safe_margin = 0.001
-	Mypersrandom = rng.randi_range(1 , 99) # used for animations we dont want synced
+	setupculor()
 
-func runmove():
+
+func setupculor():
+	safe_margin = 0.001
+	motion_mode = MotionMode.MOTION_MODE_FLOATING
+	Mypersrandom = rng.randi_range(1 , 99) # used for animations we dont want synced
+	
+	#setup animator
+	add_child(Animator)
+	Animator.add_animation_library("",load("res://CulorAnimations.tres"))
+	
+	#setup attack timer
+	add_child(attackTimer)
+	
+	#if a extended has ready
+	if has_method("ready2"):
+		call("ready2")
+
+func runmove(delta):
 	if targetposition:
 		if position.distance_to(targetposition) >= targetcutoff:
-			velocity.x = cos(position.angle_to_point(targetposition)) * speed
-			velocity.y = sin(position.angle_to_point(targetposition)) * speed
+			velocity = velocity.move_toward(targetposition - position,speed * delta)
+		else:
+			velocity = velocity.move_toward(Vector2.ZERO,speed * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO,speed * delta)
 	move_and_slide()
-	velocity.x *= 0.9
-	velocity.y *= 0.9
 
-func _process(delta):
+func loopUntilRange():
+	while position.distance_to(targetposition) >= targetcutoff:
+		await get_tree().physics_frame
+	return
+	
+
+func _physics_process(delta):
 	#MOVE!!
-	runmove()
+	runmove(delta)
+	velocity.move_toward(Vector2.DOWN,speed) * 34
+	
+	if !Animator.is_playing():
+		Animator.play("idle")
+	
+	#visualize move with anim
+	if Vector2(round(position.x * 10) / 10,round(position.y * 10)/ 10)  != lastpos:
+		if !walkinganimstate:
+			walkinganimstate = true
+			Animator.play("walk")
+	else:
+		if walkinganimstate:
+			walkinganimstate = false
+			Animator.play("walkend",-1,1,true)
+	
+	if velocity.x != 0:
+		if sign(velocity.x) == -1:
+			$Texture.flip_h = true
+		else:
+			$Texture.flip_h = false
 	
 	# run modulation animation for a selected culor
 	Timesincestart += delta
 	if is_in_group("Selected_Culor"):
 		var animationvalue = 0.75+ (sin(Timesincestart * 5) * 0.25)
 		modulate = Color(animationvalue,animationvalue,animationvalue)
+	lastpos.x = round(position.x * 10) / 10
+	lastpos.y = round(position.y * 10) / 10
 
 func select():
 	if not is_in_group("Selected_Culor"):
@@ -54,5 +106,4 @@ func move(pos : Vector2):
 
 func target(Enemy : BaseEnemy):
 	move(Enemy.global_position)
-	targetcutoff = range
-	print("GETOU-")
+	targetcutoff = attackRange
--- Scripts/Melee.gd
+++ Scripts/Melee.gd
@@ -1,3 +1,23 @@
 extends BaseCulor
+class_name MeleeCulor
+
+@export var damage = 4
+
+var targeted = false
+var TargetEnemy
+
 func target(Enemy : BaseEnemy):
-	pass
+	move(Enemy.global_position)
+	targetcutoff = attackRange
+
+func _ready():
+	setupculor()
+	
+	attackTimer.wait_time = attackCooldown
+	attackTimer.timeout.connect(attacktimerend)
+	attackTimer.start()
+
+func attacktimerend():
+	await loopUntilRange()
+	Animator.play("attack_melee")
+	attackTimer.start()
