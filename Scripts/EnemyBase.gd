debda43ac35102f0323eb5dedf096ee984b1e450
Regonold <regonold520@gmail.com>
2024-12-23 01:06:58 +0000

    added base enemy funcs

--- Scripts/EnemyBase.gd
+++ Scripts/EnemyBase.gd
@@ -3,3 +3,14 @@ class_name BaseEnemy
 
 @export var speed = 20
 @export var health = 20
+
+func move():
+	pass
+
+func attack():
+	pass
+
+
+
+func _on_culor_detection_entered(body: Node2D) -> void:
+	pass # Replace with function body.
